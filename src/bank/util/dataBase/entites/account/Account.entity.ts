04afee80dfcd87a35f53176c35d62dcd3024fb2b
Juice <3221ju@gmail.com>
2022-07-30 03:21:01 +0900

    Account 추가

--- src/bank/util/dataBase/entites/account/Account.entity.ts
+++ src/bank/util/dataBase/entites/account/Account.entity.ts
@@ -1,10 +1,13 @@
-import { Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { Client } from '../client/client.entity';
 
 @Entity()
 export class Account {
   @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({ type: 'text', nullable: false, comment: '계좌 개설 목적' })
+  purposeDescription;
 
   @ManyToOne(() => Client, (client) => client.accounts)
   client: Client[];
---
+++ src/bank/util/dataBase/entites/account/AccountModule.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Account } from './Account.entity';
+
+@Module({
+  imports: [TypeOrmModule],
+  exports: [TypeOrmModule.forFeature([Account])],
+})
+export class AccountModule {}
