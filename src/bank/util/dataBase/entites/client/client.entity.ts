356f72ed7a5963a9768190c46ac083f615381802
Juice <3221ju@gmail.com>
2022-07-30 04:44:27 +0900

    이메일 unique 이름 변경

--- src/bank/util/dataBase/entites/client/client.entity.ts
+++ src/bank/util/dataBase/entites/client/client.entity.ts
@@ -1,15 +1,18 @@
-import { Column, Entity, OneToMany } from 'typeorm';
+import { Column, Entity, OneToMany, Unique } from 'typeorm';
 import { Account } from '../account/Account.entity';
 import { IsEmail } from 'class-validator';
 import { BaseTable } from '../../BaseEntities/BaseTable/BaseTable';
 
 @Entity()
+@Unique(`unique_${Client.tableName}_email_constraint`, ['email'])
 export class Client extends BaseTable {
+  static tableName = 'client';
+
   @Column({ length: 50, nullable: false })
   // TODO 한/영 1~25자 제한 클래스 밸리데이터 추가하기
   name: string;
 
-  @Column({ length: 100, nullable: false, unique: true })
+  @Column({ length: 100, nullable: false })
   @IsEmail()
   email: string;
 
