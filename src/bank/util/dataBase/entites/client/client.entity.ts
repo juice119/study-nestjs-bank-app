feb5aaabebfc65fc62b0466dee7bc73c6c1754a5
Juice <3221ju@gmail.com>
2022-07-30 03:17:35 +0900

    Client 수정

--- src/bank/util/dataBase/entites/client/client.entity.ts
+++ src/bank/util/dataBase/entites/client/client.entity.ts
@@ -1,13 +1,19 @@
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { Account } from '../account/Account.entity';
+import { IsEmail } from 'class-validator';
 
 @Entity()
 export class Client {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ length: 100 })
+  @Column({ length: 50, nullable: false })
+  // TODO 한/영 1~25자 제한 클래스 밸리데이터 추가하기
   name: string;
+
+  @Column({ length: 100, nullable: false, unique: true })
+  @IsEmail()
+  email: string;
 
   @Column({
     type: 'text',
